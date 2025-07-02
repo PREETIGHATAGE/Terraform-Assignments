5772be63459d9ba2351d3f6f2e2fd8271d72d984
PREETIGHATAGE <164339733+PREETIGHATAGE@users.noreply.github.com>
2025-07-02 21:04:48 +0530

    Update datazone.tf

--- TerraformAssignments/datazone.tf
+++ TerraformAssignments/datazone.tf
@@ -1,7 +1,7 @@
-/*module "datazone" {
+module "datazone" {
   source = "./modules/datazone"
 
   datazone_name = var.datazone_name
   bucket_prefix = var.bucket_prefix
   tags          = var.tags
-}*/
+}
